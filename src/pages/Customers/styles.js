f61d2bcd9672dcfbc8c600ad3573d6c0bfc71020
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-05-10 18:23:25 -0300

    create customers page

---
+++ src/pages/Customers/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components/macro";
+
+export const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Body = styled.div``;
+
+export const Section = styled.section`
+  margin: 20px 0;
+`;
+
+export const CardsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: start;
+`;
