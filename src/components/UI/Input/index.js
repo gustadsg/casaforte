e221dd4efcd725693fec6b84a9f0837f4de3a05d
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-05-11 01:40:17 -0300

    improve input

--- src/components/UI/Input/index.js
+++ src/components/UI/Input/index.js
@@ -2,14 +2,34 @@ import React from "react";
 import styled from "styled-components";
 import { Icon } from "..";
 
-export default function Input({ suffix, width, value, onChange }) {
+export default function Input({
+  suffix,
+  width,
+  value,
+  onChange,
+  label,
+  margin,
+  required,
+  ...rest
+}) {
   return (
-    <Input.Wrapper width={width}>
-      <Input.Input />
-      {suffix && <Icon src={suffix} size="16px" style={{ marginRight: 10 }} />}
-    </Input.Wrapper>
+    <Label margin={margin}>
+      {label}
+      {required && <Input.Required> *</Input.Required>}
+      <Input.Wrapper width={width}>
+        <Input.Input onChange={onChange} value={value} {...rest} />
+        {suffix && (
+          <Icon src={suffix} size="16px" style={{ marginRight: 10 }} />
+        )}
+      </Input.Wrapper>
+    </Label>
   );
 }
+
+const Label = styled.label`
+  color: ${({ theme }) => theme.grey};
+  font-weight: ${({ theme }) => theme.bold};
+`;
 
 Input.Wrapper = styled.div`
   border: ${({ theme }) => `solid 2px ${theme.grey}`};
@@ -18,9 +38,11 @@ Input.Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  margin-top: 3px;
 
   input:focus + & {
     border: ${({ theme }) => `solid 1px ${theme.primary}`};
+    height: 100%;
   }
 `;
 
@@ -38,4 +60,8 @@ Input.Input = styled.input`
     border: none;
     outline: none;
   }
+`;
+
+Input.Required = styled.span`
+  color: red;
 `;
