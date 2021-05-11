200534acfd0e2128bbc8ff40f7243b0efa770532
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-05-11 01:39:53 -0300

    export loading

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as CustomerCard } from "./CustomerCard";
 export { default as PageWrapper } from "./PageWrapper";
+export { default as Loading } from "./Loading";
 export * from "./UI";
