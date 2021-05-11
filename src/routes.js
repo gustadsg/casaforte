c68767584c0e40344328e5591c9b78d3ae6ac35f
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-05-11 01:46:07 -0300

    create route for register

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { Customers } from "./pages";
+import { Customers, NewCustomer } from "./pages";
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact component={Customers} path="/" />
+        <Route exact component={NewCustomer} path="/customer/register" />
       </Switch>
     </BrowserRouter>
   );
