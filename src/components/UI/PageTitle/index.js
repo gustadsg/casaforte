ab4ae0914682f4b1ea3e66d37ccdfcf61845169c
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-05-12 14:14:32 -0300

    add font to page title component

--- src/components/UI/PageTitle/index.js
+++ src/components/UI/PageTitle/index.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const PageTitle = styled.h1`
   color: ${({ theme }) => theme.secondary};
+  font-family: ${({ theme }) => theme.font};
 `;
 
 export default PageTitle;
