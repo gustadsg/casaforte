eff18fcb3490e955838709a058c40aec68d215e1
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-05-11 21:52:38 -0300

    (in progress) create TabPanel

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,6 @@ export { default as CustomerCard } from "./CustomerCard";
 export { default as Loading } from "./Loading";
 export { default as PageWrapper } from "./PageWrapper";
 export { default as ReferenceCard } from "./ReferenceCard";
+export { default as Navbar } from "./Navbar";
+export { default as TabPanel } from "./TabPanel";
 export * from "./UI";
