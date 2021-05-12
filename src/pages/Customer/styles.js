be796dcb17f931de91d26fc45b40cfcd6a515721
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-05-12 14:17:22 -0300

    styles for customer page

--- src/pages/Customer/styles.js
+++ src/pages/Customer/styles.js
@@ -1,3 +1,35 @@
 import styled from "styled-components";
+import { ReferenceCard } from "../../components";
 
 export const Wrapper = styled.div``;
+
+export const Divided = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  right: 0;
+
+  & > label {
+    margin-top: 20px;
+  }
+`;
+
+export const CardsContainer = styled.div`
+  margin: 20px 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  overflow-x: scroll;
+
+  padding: 20px 40px 20px 20px;
+`;
+
+export const SReferenceCard = styled(ReferenceCard)`
+  margin-right: 20px;
+`;
